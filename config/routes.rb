c5dde565730b025553ffd3384237e98c862fcb1e
Lucas Braun <lucasalan@gmail.com>
2018-10-09 12:25:23 +0200

    Add welcome controller and basic index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :location_records
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  root 'welcome#index'
+
+  resources :location_records
 end
